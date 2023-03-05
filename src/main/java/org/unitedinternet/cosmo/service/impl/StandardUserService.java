5aac11760083ab7032e688f4914de93b11fd524f
mirko <mirko.perillo@gmail.com>
2023-03-05 22:35:05 +0100

    Improve default calendar and addressbook display name

--- src/main/java/org/unitedinternet/cosmo/service/impl/StandardUserService.java
+++ src/main/java/org/unitedinternet/cosmo/service/impl/StandardUserService.java
@@ -1,13 +1,14 @@
 package org.unitedinternet.cosmo.service.impl;
 
-import carldav.entity.CollectionItem;
-import carldav.entity.User;
-import carldav.repository.UserRepository;
+import java.util.Objects;
+
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.unitedinternet.cosmo.service.ContentService;
 import org.unitedinternet.cosmo.service.UserService;
 
-import java.util.Objects;
+import carldav.entity.CollectionItem;
+import carldav.entity.User;
+import carldav.repository.UserRepository;
 
 public class StandardUserService implements UserService {
 
@@ -16,10 +17,9 @@ public class StandardUserService implements UserService {
   private final PasswordEncoder passwordEncoder;
 
   public StandardUserService(
-    ContentService contentService,
-    UserRepository userRepository,
-    PasswordEncoder passwordEncoder
-  ) {
+      ContentService contentService,
+      UserRepository userRepository,
+      PasswordEncoder passwordEncoder) {
     this.contentService = Objects.requireNonNull(contentService, "contentService is null");
     this.userRepository = Objects.requireNonNull(userRepository, "userRepository is null");
     this.passwordEncoder = Objects.requireNonNull(passwordEncoder, "passwordEncoder is null");
@@ -33,7 +33,7 @@ public class StandardUserService implements UserService {
     var calendar = new CollectionItem();
     calendar.setOwnerId(user.getId());
     calendar.setName("calendar");
-    calendar.setDisplayName("calendarDisplayName");
+    calendar.setDisplayName("%s's calendar".formatted(user.getEmail()));
 
     var homeCollection = contentService.createRootItem(newUser);
     contentService.createCollection(homeCollection, calendar);
@@ -41,7 +41,7 @@ public class StandardUserService implements UserService {
     var addressbook = new CollectionItem();
     addressbook.setOwnerId(user.getId());
     addressbook.setName("contacts");
-    addressbook.setDisplayName("contactDisplayName");
+    addressbook.setDisplayName("%s's contacts".formatted(user.getEmail()));
 
     contentService.createCollection(homeCollection, addressbook);
 
